5564982924aa048484c9e5b0ed3fe52ca7efd4f2
vkguru <vnwoguru@gmail.com>
2020-10-25 15:47:04 +0100

    create table and render data in table

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Nav from './components/nav';
 import SideMenu from './components/sideMenu';
 import Trans from './components/trans';
-import Summary from './components/summary'
+import Summary from './components/summary';
+import Payment from './components/payment';
 import './asset/css/style.css';
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <main className="main">
         <Trans />
         <Summary />
+        <Payment />
       </main>
     </>
   );
